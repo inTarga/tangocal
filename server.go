b3fa3c09076f9ce4b7766c3db087e592dfaeafe7
Ingrid <intarga@protonmail.com>
2019-06-12 14:03:46 +0200

    implemented postgres event database

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-pg/pg"
+	//"github.com/go-pg/pg/orm"
 	"github.com/labstack/echo"
 	"net/http"
 )
@@ -14,15 +17,64 @@ type Event struct {
 
 type Events []Event
 
-func hwhandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type repo struct {
+	db *pg.DB
 }
 
-func indexHandler(c echo.Context) error {
-	return c.File("view/index.html")
+func (repo *repo) getJson(c echo.Context) error {
+	var events Events
+	err := repo.db.Model(&events).Select()
+	if err != nil {
+		fmt.Println("getJsonErr")
+		return err
+	}
+	fmt.Println(events)
+	return c.JSON(http.StatusOK, events)
 }
 
-func getJson(c echo.Context) error {
+func (repo *repo) restartSchema() error {
+	_, err := repo.db.Exec(`
+		DROP SCHEMA public CASCADE;
+		CREATE SCHEMA public;
+		CREATE TABLE events
+		(
+			title 	text,
+			link 	text,
+			start 	text
+		);
+	`)
+	return err
+}
+
+func (repo *repo) addEvent(event Event) error { //can in favour of just orming directly?
+	err := repo.db.Insert(&event)
+	if err != nil {
+		fmt.Println("insert err")
+		fmt.Println(err)
+	}
+	return err
+}
+
+func (repo *repo) addEvents(events Events) error {
+	var err error
+	for i := 0; i < len(events); i++ {
+		err = repo.addEvent(events[i])
+	}
+	return err
+}
+
+func main() {
+	e := echo.New()
+
+	repo := repo{
+		pg.Connect(&pg.Options{
+			User:     "postgres",
+			Password: "",
+			Database: "tangodb",
+		}),
+	}
+	repo.restartSchema() //remove for persistent schema
+
 	var events = Events{
 		Event{
 			"event1",
@@ -40,13 +92,10 @@ func getJson(c echo.Context) error {
 			"2019-06-18",
 		},
 	}
-	return c.JSON(http.StatusOK, events)
-}
+	repo.addEvents(events)
 
-func main() {
-	e := echo.New()
-	//e.GET("/", indexHandler)
 	e.Static("/", "view")
-	e.GET("/jsonevent", getJson)
+	e.GET("/jsonevent", repo.getJson)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
